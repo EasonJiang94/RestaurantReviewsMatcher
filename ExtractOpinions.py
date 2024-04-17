4dae15bb7321fa9586d0837113b375061dc08bf3
easonjiang94 <easonjiang94@gmail.com>
2024-04-16 22:48:57 -0400

    init

--- ExtractOpinions.py
+++ ExtractOpinions.py
@@ -20,34 +20,19 @@ class ExtractOpinions:
     # Extracted opinions and corresponding review id is saved in extracted_pairs, where KEY is the opinion and VALUE
     # is the set of review_ids where the opinion is extracted from.
     # Opinion should in form of "attribute, assessment", such as "service, good".
+    extracted_opinions = {}
 
     def __init__(self):
-        # Initialize the NLP pipeline
-        self.nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
-        self.extracted_opinions = {}
+        try : 
+            self.nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
+        except :
+            stanza.download('en')
+            self.nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,depparse')
+        return
 
     def extract_pairs(self, review_id, review_content):
-        # Analyze the review content using NLP to extract opinions
-        doc = self.nlp(review_content)
-        for sentence in doc.sentences:
-            for word in sentence.words:
-                # Check for adjective modifiers (amod) related to a noun
-                if word.deprel == 'amod' and sentence.words[word.head - 1].upos == 'NOUN':
-                    attribute = sentence.words[word.head - 1].lemma
-                    assessment = word.lemma
-                    opinion = f"{attribute}, {assessment}"
-                    if opinion in self.extracted_opinions:
-                        if review_id not in self.extracted_opinions[opinion]:
-                            self.extracted_opinions[opinion].append(review_id)
-                    else:
-                        self.extracted_opinions[opinion] = [review_id]
-
-                # You might want to expand this section with more rules depending on the NLP analysis
-                # For example, handling nominal subjects or compound modifiers
-
-    def add_review(self, review_id, review_content):
-        # This method will be called to process each review
-        self.extract_pairs(review_id, review_content)
+        # example data, which you will need to remove in your real code. Only for demo.
+        self.extracted_opinions = {'service, good': [1, 2, 5], 'service, excellent': [4, 6]}
 
 
 if __name__ == "__main__":
